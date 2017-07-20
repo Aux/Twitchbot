941b4c932cb022945103dcc4e620ce7ea63814c1
Aux <justin.gentry@auxesis.tv>
2017-07-20 15:22:15 -0500

    initial

---
+++ src/TwitchSelfbot/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TwitchSelfbot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+            => new Program().StartAsync().GetAwaiter().GetResult();
+
+        public async Task StartAsync()
+        {
+            await Task.Delay(0);
+        }
+    }
+}
